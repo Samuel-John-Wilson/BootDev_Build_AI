c33052992266ff572fcf4f78a77c996f17a0a928
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-06-28 20:58:29 +1000

    0.0.5: commandline input extended, ch2 l2 begun

--- src/main.py
+++ src/main.py
@@ -13,16 +13,19 @@ client = genai.Client(api_key=api_key)
 def main():
     if len(sys.argv)<2:
         print("Error: Input prompt not provided") # Exit if no prompt!
+        return
+    verbose_flag = "--verbose" in sys.argv
+
     
-    user_prompt = sys.argv[1:] # Extract user prompt from command line input 
+    user_prompt = sys.argv[1] # Extract user prompt from command line input 
     # Create messages list 
     messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])] 
-
-
     response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose_flag:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__=="__main__":
