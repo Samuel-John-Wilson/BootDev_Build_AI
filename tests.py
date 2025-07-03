b78ed719a53df1d54bda9df1063f49a8dbbcac82
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-07-03 16:28:03 +1000

    0.1.0: changed to uv package manager. get_file_content working

--- tests.py
+++ tests.py
@@ -2,7 +2,10 @@ from functions.get_files_info import get_files_info, is_subdirectory
 from functions.get_file_content import get_file_content
 
 def main():
-    get_file_content(calculator, lorem.txt)
+   print(get_file_content("calculator", "main.py"))
+   print(get_file_content("calculator", "pkg/calculator.py"))
+   print(get_file_content("calculator", "/bin/cat"))
+   
 
     
 
@@ -17,6 +20,8 @@ if __name__=="__main__":
 
     #old tests
     """
+   print(get_file_content('calculator', 'lorem.txt'))
+
     print(get_files_info("calculator", "."))
     print(get_files_info("calculator", "pkg"))
     print(get_files_info("calculator", "/bin"))
