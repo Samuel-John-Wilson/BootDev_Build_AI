1e275423967dabacbc2a06851309d6fd6d8fa239
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-07-08 15:54:55 +1000

    0.3.1: began call_function

---
+++ functions/call_function.py
@@ -0,0 +1,13 @@
+from google import genai
+from google.genai import types
+import sys
+
+
+
+def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+    
+    
