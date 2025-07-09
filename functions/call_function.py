be7b622db95d740a7d4be7d2cace8f9a3a85a98e
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-07-09 18:57:15 +1000

    0.3.2: call_function progress

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,7 +1,10 @@
 from google import genai
 from google.genai import types
 import sys
-
+from functions.get_files_info import schema_get_files_info, get_files_info, is_subdirectory
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python import run_python_file, schema_run_python_file
+from functions.write_file import write_file, schema_write_file
 
 
 def call_function(function_call_part, verbose=False):
@@ -10,4 +13,20 @@ def call_function(function_call_part, verbose=False):
     else:
         print(f" - Calling function: {function_call_part.name}")
     
-    
+    function_map = {"get_files_info" : get_files_info, "get_file_content" : get_file_content, "run_python_file" : run_python_file, "write_file" : write_file}
+
+    arguments_map = {"working_directory" : "./calculator", **function_call_part.args}
+
+    if not function_call_part.name in function_map:
+        return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_call_part.name,
+            response={"error": f"Unknown function: {function_call_part.name}"},
+        )
+    ],
+        )
+    else:
+        function = function_map[function_call_part.name]
+        function(**function_map)
